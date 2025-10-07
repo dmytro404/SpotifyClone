e2d28b73a021c866742908433c68c06b5ae4bff1
Dmytro <dberezhkov2016@gmail.com>
2025-10-07 21:13:11 +0300

    Added Rest model

---
+++ Models/Rest/RestResponce.cs
@@ -0,0 +1,10 @@
+﻿namespace SpotifyClone.Models.Rest
+{
+    public class RestResponce
+    {
+        public RestStatus Status { get; set; } = new();
+        public RestMeta Meta { get; set; } = new();
+
+        public Object? Data { get; set; }
+    }
+}
