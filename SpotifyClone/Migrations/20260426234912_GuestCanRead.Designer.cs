7aa9badcd973540082191d33f34250e3d1e3d64e
NotVerySmart123221 <sashatop4ik23@gmail.com>
2026-04-27 11:51:25 +1200

    changed rights

--- SpotifyClone/Migrations/20260426234912_GuestCanRead.Designer.cs
+++ SpotifyClone/Migrations/20260426234912_GuestCanRead.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SpotifyClone.Data;
 
@@ -11,9 +12,11 @@ using SpotifyClone.Data;
 namespace SpotifyClone.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20260426234912_GuestCanRead")]
+    partial class GuestCanRead
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
